fa0579076868083d5d85c2b35f87478371817a35
vitaly <roslov@lanit.ru>
2018-08-13 13:27:22 +0300

    implementing parsing

--- parser/src/main/kotlin/vitalyros/diceformula/parser/ParserImpl.kt
+++ parser/src/main/kotlin/vitalyros/diceformula/parser/ParserImpl.kt
@@ -15,19 +15,22 @@ class ParserImpl : Parser {
         when (token.Type) {
             TokenType.INT -> {
                 finishedStack.push(parseInt(token))
-                checkFinishedPlusMinus()
             }
             TokenType.DICE -> {
                 checkMultSugar()
                 finishedStack.push(parseDice(token))
-                collapsePendingMult()
-                checkFinishedPlusMinus()
+                collapseMult()
+                collapsePlusMinus()
             }
             TokenType.PLUS -> {
+                collapseIntMult()
+                collapsePlusMinus()
                 val lastFinished = finishedStack.peek()
                 pendingStack.push(PendingSum(validatePlusMinus(token, lastFinished)))
             }
             TokenType.MINUS -> {
+                collapseIntMult()
+                collapsePlusMinus()
                 val lastFinished = finishedStack.peek()
                 pendingStack.push(PendingDif(validatePlusMinus(token, lastFinished)))
             }
@@ -40,9 +43,11 @@ class ParserImpl : Parser {
                 pendingStack.push(PendingBraces(token))
             }
             TokenType.CLOSE_BRACE -> {
-                checkFinishedBraces()
-                checkFinishedPlusMinus()
-                checkFinishedMult()
+                collapseIntMult()
+                collapsePlusMinus()
+                collapseBraces()
+                collapseMult()
+                collapsePlusMinus()
             }
             TokenType.MULT -> {
                 pendingStack.push(parseMult(token))
@@ -50,110 +55,11 @@ class ParserImpl : Parser {
         }
     }
 
-    fun checkMultSugar() {
-        val lastFinished = finishedStack.peek()
-        if (lastFinished != null && lastFinished is IntLiteral) {
-            finishedStack.pop()
-            pendingStack.push(PendingMult(lastFinished.value))
-        }
-    }
-
-    fun checkFinishedMult() {
-        val exp = finishedStack.peek()
-        if (exp != null && exp !is PendingExpression) {
-            checkFinishedMult(exp)
-        }
-    }
-
-    fun checkFinishedMult(exp: Expression) {
-        if (pendingStack.size > 0) {
-            val lastPending = pendingStack.peek()
-            when (lastPending) {
-                is PendingMult -> {
-                    finishedStack.push(Mult(lastPending.mult, exp))
-                    pendingStack.pop()
-                }
-            }
-        }
-    }
-
-    fun checkFinishedBraces() {
-        val exp = finishedStack.peek()
-        if (exp != null && exp !is PendingExpression) {
-            checkFinishedBraces(exp)
-        }
-    }
-
-    fun checkFinishedBraces(exp: Expression) {
-        if (pendingStack.size > 0) {
-            val lastPending = pendingStack.peek()
-            when (lastPending) {
-                is PendingBraces -> {
-                    finishedStack.push(Braces(exp))
-                    pendingStack.pop()
-                }
-                is PendingFun -> {
-                    finishedStack.push(Fun(lastPending.name, exp))
-                    pendingStack.pop()
-                }
-            }
-        }
-    }
-
-    fun collapsePendingMult() {
-        var finishedCollapsing = false
-        while (!finishedCollapsing && pendingStack.size > 0) {
-            val depthBefore = pendingStack.size
-
-            val lastFinished = finishedStack.peek()
-            val lastPending = pendingStack.peek()
-            if (lastFinished != null) {
-                when (lastPending) {
-                    is PendingMult -> {
-                        finishedStack.push(Braces(lastFinished))
-                        pendingStack.pop()
-                    }
-                }
-            }
-
-            val depthAfter = pendingStack.size
-            finishedCollapsing = depthAfter == depthBefore
-        }
-    }
-
-
-    fun checkFinishedPlusMinus() {
-        val exp2 = finishedStack.peek()
-        if (exp2 != null && exp2 !is PendingExpression) {
-            checkFinishedPlusMinus(exp2)
-        }
-    }
-
-    fun checkFinishedPlusMinus(exp2: Expression) {
-        if (pendingStack.size > 0) {
-            val lastPending = pendingStack.peek()
-            when (lastPending) {
-                is PendingSum -> {
-                    finishedStack.push(Sum(lastPending.exp1, exp2))
-                    pendingStack.pop()
-                }
-                is PendingDif -> {
-                    finishedStack.push(Dif(lastPending.exp1, exp2))
-                    pendingStack.pop()
-                }
-            }
-        }
-    }
-
-    fun validatePlusMinus(token: Token, lastFinished: Expression?) : Expression {
-        if (lastFinished != null && lastFinished !is PendingExpression) {
-            return lastFinished
-        } else {
-            throw ParserException("Finished expression expected on the left side of ${token.str}")
-        }
-    }
-
     override fun finish(): Expression {
+        // Expression may end on unprocessed int literal, process it here
+        collapseIntMult()
+        collapsePlusMinus()
+        // The top of the finished stack should be the result
         val result = finishedStack.peek()
         if (result == null) {
             if (pendingStack.isEmpty()) {
@@ -166,14 +72,114 @@ class ParserImpl : Parser {
         }
     }
 
+    /**
+     * Checks the last finished expression to be int and collapses all preceding unfinished multiplication expressions, if they exist
+     */
+    fun collapseIntMult() {
+        val lastFinished = finishedStack.peek()
+        if (lastFinished != null && lastFinished is IntLiteral) {
+            collapseMult()
+        }
+    }
+
+
+    /**
+     * Checks the last finished expression to be int and substitutes it for unfinished multiplication expression
+     */
+    fun checkMultSugar() {
+        val lastFinished = finishedStack.peek()
+        if (lastFinished != null && lastFinished is IntLiteral) {
+            finishedStack.pop()
+            pendingStack.push(PendingMult(lastFinished.value))
+        }
+    }
+
+    /**
+     * Closes immediate pending open braces or function expressions
+     */
+    fun collapseBraces() {
+        val exp = finishedStack.peek()
+        if (exp != null) {
+            if (pendingStack.size > 0) {
+                val lastPending = pendingStack.peek()
+                when (lastPending) {
+                    is PendingBraces -> {
+                        finishedStack.push(Braces(exp))
+                        pendingStack.pop()
+                    }
+                    is PendingFun -> {
+                        finishedStack.push(Fun(lastPending.name, exp))
+                        pendingStack.pop()
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Collapses the chain of unfinished multiplication expressions
+     */
+    fun collapseMult() {
+        var finishedCollapsing = false
+        while (!finishedCollapsing && pendingStack.size > 0) {
+            val depthBefore = pendingStack.size
+
+            val lastFinished = finishedStack.peek()
+            val lastPending = pendingStack.peek()
+            if (lastFinished != null) {
+                when (lastPending) {
+                    is PendingMult -> {
+                        finishedStack.push(Mult(lastPending.mult, lastFinished))
+                        pendingStack.pop()
+                    }
+                }
+            }
+
+            val depthAfter = pendingStack.size
+            finishedCollapsing = depthAfter == depthBefore
+        }
+    }
+
+    /**
+     * Closes immediate pending sum or dif expression
+     */
+    fun collapsePlusMinus() {
+        val exp2 = finishedStack.peek()
+        if (exp2 != null) {
+            if (pendingStack.size > 0) {
+                val lastPending = pendingStack.peek()
+                when (lastPending) {
+                    is PendingSum -> {
+                        finishedStack.push(Sum(lastPending.exp1, exp2))
+                        pendingStack.pop()
+                    }
+                    is PendingDif -> {
+                        finishedStack.push(Dif(lastPending.exp1, exp2))
+                        pendingStack.pop()
+                    }
+                }
+            }
+        }
+    }
+
+    fun validatePlusMinus(token: Token, lastFinished: Expression?): Expression {
+        if (lastFinished != null && lastFinished !is PendingExpression) {
+            return lastFinished
+        } else {
+            throw ParserException("Finished expression expected on the left side of ${token.str}")
+        }
+    }
+
+
     private fun parseInt(token: Token) = IntLiteral(token.str.toInt())
     private fun parseDice(token: Token) = DiceLiteral(token.str.substring(1).toInt())
     private fun parseFun(token: Token) = PendingFun(token.str.filter { it in funNameChars })
-    private fun parseMult(token: Token) = PendingMult((token.str.filter { it in intChars}).toInt())
+    private fun parseMult(token: Token) = PendingMult((token.str.filter { it in intChars }).toInt())
 }
+
 private interface PendingExpression : Expression
 private data class PendingSum(val exp1: Expression) : PendingExpression
 private data class PendingDif(val exp1: Expression) : PendingExpression
-private data class PendingFun(val name: String): PendingExpression
+private data class PendingFun(val name: String) : PendingExpression
 private data class PendingBraces(val token: Token) : PendingExpression
 private data class PendingMult(val mult: Int) : PendingExpression
