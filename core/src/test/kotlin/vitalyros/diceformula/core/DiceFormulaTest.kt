320ccc79aae15bae69359d317307a88f97bc79dc
vitaly <roslov@lanit.ru>
2018-09-06 12:22:24 +0300

    run the whole thing at last

--- core/src/test/kotlin/vitalyros/diceformula/core/DiceFormulaTest.kt
+++ core/src/test/kotlin/vitalyros/diceformula/core/DiceFormulaTest.kt
@@ -19,4 +19,17 @@ class DiceFormulaTest {
         `when`(diceRoller.roll(6)).thenReturn(3)
         Assert.assertEquals(18, DiceFormula.exec("d20 + d6 + 5".toByteArray(), diceRoller))
     }
+
+    @Test
+    fun functionTest_max() {
+        `when`(diceRoller.roll(20)).thenReturn(10,  15, 6)
+        `when`(diceRoller.roll(6)).thenReturn(3, 6, 5)
+        Assert.assertEquals(26, DiceFormula.exec("max(3 * (d20 + d6 + 5))".toByteArray(), diceRoller))
+    }
+    @Test
+    fun functionTest_min() {
+        `when`(diceRoller.roll(20)).thenReturn(10,  15, 6)
+        `when`(diceRoller.roll(6)).thenReturn(3, 6, 5)
+        Assert.assertEquals(16, DiceFormula.exec("min(3 * (d20 + d6 + 5))".toByteArray(), diceRoller))
+    }
 }
